7084cc58b89d0c23ee8dd5a7f5f4c222a15842d6
iammrgenie <frimpong.5909311@gmail.com>
2021-10-18 17:10:41 +0300

    Final Test

--- ED-ainq.c
+++ ED-ainq.c
@@ -314,7 +314,7 @@ int main(int argc, char *argv[])
 	puts("Socket Created");
 
 	//initialize connection parameters
-	server.sin_addr.s_addr = inet_addr("127.0.0.1");
+	server.sin_addr.s_addr = inet_addr("172.18.32.117");
 	server.sin_family = AF_INET;
 	server.sin_port = htons(5555);
 
